bfcc7f1f7bad657aa7aaa4b8a562913cf123d04e
C0mplex <gaybullayevodiljon@gmail.com>
2023-06-10 15:26:40 +0500

    w

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 // @dart=2.9
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:easy_sidemenu/easy_sidemenu.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tests/navbar.dart';
 import 'package:flutter_tests/screens/group/components/body.dart';
-import 'package:flutter_tests/screens/group/group_screen.dart';
-import 'package:flutter_tests/screens/home/home_screen.dart';
 import 'package:flutter_tests/splash_screen.dart';
-import 'package:splashscreen/splashscreen.dart';
-import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(SplashApp());
