503853ce5511d9183a4bc85da258ddd59cf28be4
C0mplex <gaybullayevodiljon@gmail.com>
2024-01-10 10:18:38 +0500

    w

--- web/flutter.js
+++ web/flutter.js
@@ -1,6 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// Import the functions you need from the SDKs you need
 
 if (!_flutter) {
     var _flutter = {};
